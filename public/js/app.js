cae658607f9b750b9a980e96f27eef1b51baf98c
Teun Huibregtse <huib0029@hz.nl>
2017-12-12 15:10:33 +0100

    Api publieke data multifunctioneel gemaakt met overerving etc #15

--- public/js/app.js
+++ public/js/app.js
@@ -65700,43 +65700,44 @@ angular.module('LaravelAngular').controller('TaskController', ['$scope', '$http'
 // API controller voor de API's
 angular.module('LaravelAngular').controller('APIController', ['$scope', '$http', function ($scope, $http) {
 
-  // Scope instellen voor webpagina
-  $scope.api = [];
-  // Functie maken die een API call oproepts
-  $scope.ZoekOpleidingsVarianten = function (index) {
-    // invoervelden van api.blade.php doorvoeren naar url van http get
-    var statusid = document.getElementById('inputStatusid_opleidingsvarianten').value;
-    var taalid = document.getElementById('inputTaalid_opleidingsvarianten').value;
-    // API call naar publieke endpoint voor opleidingsvarianten
-    $http({
-      method: 'GET',
-      dataType: "Json",
-      url: 'https://apps.hz.nl/Services/algemeen/v1/opleidingsvarianten?statusid=' + statusid + '&taalid=' + taalid
-    }).then(function (response) {
-      // array list maken voor api blade met ng-repeat en data uit array halen
-      $scope.opleidingsvariantenapi = response.data;
-    }).catch(function (error) {
-      alert("De API functioneert niet, noteer foutcode");
-      alert(error.data);
-    });
-  };
-  $scope.ZoekCrohos = function (index) {
-    // invoervelden van api.blade.php doorvoeren naar url van http get
-    var statusid = document.getElementById('inputStatusid_crohos').value;
-    var taalid = document.getElementById('inputTaalid_crohos').value;
-    // API call naar publieke endpoint voor opleidingsvarianten
-    $http({
-      method: 'GET',
-      dataType: "Json",
-      url: 'https://apps.hz.nl/Services/algemeen/v1/crohos?statusid=' + statusid + '&taalid=' + taalid
-    }).then(function (response) {
-      // array list maken voor api blade met ng-repeat en data uit array halen
-      $scope.crohosapi = response.data;
-    }).catch(function (error) {
-      alert("De API functioneert niet, noteer foutcode");
-      alert(error.data);
-    });
-  };
+    // Scope instellen voor webpagina
+    $scope.api = [];
+    // Functie maken die een API call oproepts
+    $scope.ZoekOpleidingsVarianten = function (index) {
+        // invoervelden van api.blade.php doorvoeren naar url van http get
+        var statusid = document.getElementById('inputStatusid_opleidingsvarianten').value;
+        var taalid = document.getElementById('inputTaalid_opleidingsvarianten').value;
+        // API call naar publieke endpoint voor opleidingsvarianten
+        var urlapi = 'https://apps.hz.nl/Services/algemeen/v1/opleidingsvarianten?statusid=';
+        $scope.StartAPI(statusid, taalid, urlapi);
+    };
+    $scope.ZoekCrohos = function (index) {
+        // invoervelden van api.blade.php doorvoeren naar url van http get
+        var statusid = document.getElementById('inputStatusid_crohos').value;
+        var taalid = document.getElementById('inputTaalid_crohos').value;
+        // API call naar publieke endpoint voor crohos
+        var urlapi = 'https://apps.hz.nl/Services/algemeen/v1/crohos?statusid=';
+        $scope.StartAPI(statusid, taalid, urlapi);
+    };
+    $scope.StartAPI = function (statusid, taalid, urlapi) {
+        $http({
+            method: 'GET',
+            dataType: "Json",
+            url: urlapi + statusid + '&taalid=' + taalid
+        }).then(function (response) {
+            // array list maken voor api blade met ng-repeat en data uit array halen en in scope plaatsten
+            // op basis van geldende url
+            if (urlapi === 'https://apps.hz.nl/Services/algemeen/v1/crohos?statusid=') {
+                $scope.crohosapi = response.data;
+            }
+            if (urlapi === 'https://apps.hz.nl/Services/algemeen/v1/opleidingsvarianten?statusid=') {
+                $scope.opleidingsvariantenapi = response.data;
+            }
+        }).catch(function (error) {
+            alert("De API functioneert niet, noteer foutcode");
+            alert(error.data);
+        });
+    };
 }]);
 
 /***/ }),
--- resources/assets/js/APIControllerAngular.js
+++ resources/assets/js/APIControllerAngular.js
@@ -9,34 +9,33 @@ angular.module('LaravelAngular').controller('APIController', ['$scope', '$http',
           var statusid  = document.getElementById('inputStatusid_opleidingsvarianten').value;
           var taalid  = document.getElementById('inputTaalid_opleidingsvarianten').value;
           // API call naar publieke endpoint voor opleidingsvarianten
-          $http({
-            method: 'GET',
-            dataType: "Json",
-            url: 'https://apps.hz.nl/Services/algemeen/v1/opleidingsvarianten?statusid=' + statusid + '&taalid=' + taalid
-          }).then(function(response) {
-                // array list maken voor api blade met ng-repeat en data uit array halen
-                $scope.opleidingsvariantenapi = response.data
-          }).catch(function(error) {
-          alert("De API functioneert niet, noteer foutcode");
-          alert(error.data);
-        });
+          var urlapi = 'https://apps.hz.nl/Services/algemeen/v1/opleidingsvarianten?statusid=';
+          $scope.StartAPI(statusid, taalid, urlapi);
         }
         $scope.ZoekCrohos = function (index) {
-          // invoervelden van api.blade.php doorvoeren naar url van http get
-          var statusid  = document.getElementById('inputStatusid_crohos').value;
-          var taalid  = document.getElementById('inputTaalid_crohos').value;
-          // API call naar publieke endpoint voor opleidingsvarianten
-          $http({
-            method: 'GET',
-            dataType: "Json",
-            url: 'https://apps.hz.nl/Services/algemeen/v1/crohos?statusid=' + statusid + '&taalid=' + taalid
-          }).then(function(response) {
-                // array list maken voor api blade met ng-repeat en data uit array halen
-                $scope.crohosapi = response.data
-          }).catch(function(error) {
-          alert("De API functioneert niet, noteer foutcode");
-          alert(error.data);
-        });
+            // invoervelden van api.blade.php doorvoeren naar url van http get
+            var statusid  = document.getElementById('inputStatusid_crohos').value;
+            var taalid  = document.getElementById('inputTaalid_crohos').value;
+            // API call naar publieke endpoint voor crohos
+            var urlapi = 'https://apps.hz.nl/Services/algemeen/v1/crohos?statusid=';
+            $scope.StartAPI(statusid, taalid, urlapi);
+        }
+        $scope.StartAPI = function (statusid, taalid, urlapi) {
+            $http({
+                method: 'GET',
+                dataType: "Json",
+                url: urlapi + statusid + '&taalid=' + taalid
+            }).then(function(response) {
+                // array list maken voor api blade met ng-repeat en data uit array halen en in scope plaatsten
+                // op basis van geldende url
+                if (urlapi === 'https://apps.hz.nl/Services/algemeen/v1/crohos?statusid=') {
+                $scope.crohosapi = response.data }
+                if (urlapi === 'https://apps.hz.nl/Services/algemeen/v1/opleidingsvarianten?statusid=') {
+                    $scope.opleidingsvariantenapi = response.data }
+            }).catch(function(error) {
+                alert("De API functioneert niet, noteer foutcode");
+                alert(error.data);
+            });
         }
 
 
