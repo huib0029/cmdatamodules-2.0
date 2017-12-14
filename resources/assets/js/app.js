a932721a3e53f821d12a031e3145466075a2ec69
Teun Huibregtse <huib0029@hz.nl>
2017-12-14 13:15:47 +0100

    Kleine naams wijziging

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,5 +10,5 @@ angular.module('LaravelAngular', []
         $httpProvider.defaults.headers.post['X-CSRF-TOKEN'] = $('meta[name=csrf-token]').attr('content');
     }]);
 // laad controllers in aparte js files voor overzichtelijkheid
-require('./services/TaskControllerAngular');
-require('./services/APIControllerAngular');
+require('./servicesAngular/TaskControllerAngular');
+require('./servicesAngular/APIControllerAngular');
