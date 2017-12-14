bd28826df26659de9c56376cdea521b9b71e8a13
Teun Huibregtse <huib0029@hz.nl>
2017-12-14 12:19:57 +0100

    Api en task controller angular geplaatst in de services map

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,5 +10,5 @@ angular.module('LaravelAngular', []
         $httpProvider.defaults.headers.post['X-CSRF-TOKEN'] = $('meta[name=csrf-token]').attr('content');
     }]);
 // laad controllers in aparte js files voor overzichtelijkheid
-require('./TaskControllerAngular');
-require('./APIControllerAngular');
+require('./services/TaskControllerAngular');
+require('./services/APIControllerAngular');
