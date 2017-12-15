25543a375237980677457ff305cc1102681985e4
Teun Huibregtse <huib0029@hz.nl>
2017-12-15 22:44:16 +0100

    phpunit configuratie toegevoegd aan root voor PHPstorm

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,9 +6,9 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication
 
     {
-      
+
     }
 }
