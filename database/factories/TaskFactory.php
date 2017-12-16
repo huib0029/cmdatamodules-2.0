dac47e2f4709c4856382f4887f76a57db597b977
Teun Huibregtse <huib0029@hz.nl>
2017-12-16 13:44:09 +0100

    Unittest Task uitgebreid

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -6,6 +6,6 @@ $factory->define(App\Task::class, function (Faker $faker) {
     return [
         'user_id' => str_random(10),
         'name' => $faker->name,
-        'description' => str_random(200),
+        'description' => $faker->text(200),
     ];
 });
--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use App\Task;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class TaskTest extends TestCase
@@ -18,5 +19,16 @@ class TaskTest extends TestCase
     private static $TABLE_NAME = 'tasks';
     private static $ID1 = 1;
 
-
+    public function test_of_task_aangemaakt_kan_worden()
+    {
+        // Maak user aan d.m.v. de factory (factories/TaskFactory)
+        $task = factory(Task::class)->create();
+        // Check of er een Task aangemaakt is met de volgende variabelen van de faker uit de factory:
+        $this->assertDatabaseHas($this::$TABLE_NAME, [
+            'id'    => $this::$ID1,
+            'user_id' => $task->user_id,
+            'name' => $task->name,
+            'description' => $task->description,
+        ]);
+    }
 }
