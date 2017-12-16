72810b12b63a10e2a06b35a4e6e4992effa44532
Teun Huibregtse <huib0029@hz.nl>
2017-12-16 13:28:25 +0100

    Factory en eerste unittest aangemaakt voor Tasks #20

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Task::class, function (Faker $faker) {
+    return [
+        'user_id' => str_random(10),
+        'name' => $faker->name,
+        'description' => str_random(200),
+    ];
+});
